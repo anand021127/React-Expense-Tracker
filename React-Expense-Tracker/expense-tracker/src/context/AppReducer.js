73b91ecbd8d833b3a99f3f4a325002b7a0ec05a6
Anand Satyanarayana Bokka <133616149+anand021127@users.noreply.github.com>
2024-09-29 22:20:57 +0530

    Add files via upload

---
+++ React-Expense-Tracker/expense-tracker/src/context/AppReducer.js
@@ -0,0 +1,18 @@
+const reducers = (state, action) => {
+  switch(action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+      }
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      }
+    default:
+      return state;
+  }
+}
+
+export default reducers;
